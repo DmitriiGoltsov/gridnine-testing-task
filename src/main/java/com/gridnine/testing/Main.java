688e0a7fe40f32c6b2bb84762a2246adf1b76bf2
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2024-01-12 11:48:44 +0100

    fix: make minor improvements in readability and remove unused imports

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -1,28 +1,29 @@
 package com.gridnine.testing;
 
 import com.gridnine.testing.filtration.ArrivalBeforeDepartureCriterion;
-import com.gridnine.testing.filtration.DepartureBeforeNowCriterion;
+import com.gridnine.testing.filtration.DepartureNotBeforeNowCriterion;
 import com.gridnine.testing.filtration.FlightFiltrationCriterion;
 import com.gridnine.testing.filtration.LandTimeCriterion;
-import com.gridnine.testing.model.Flight;
 import com.gridnine.testing.service.FlightService;
 import com.gridnine.testing.service.FlightServiceImpl;
 import com.gridnine.testing.util.FlightBuilder;
+import com.gridnine.testing.util.LandTimeMode;
 
-import java.time.LocalDateTime;
 import java.util.List;
+
+import static com.gridnine.testing.util.LandTimeMode.*;
 
 public class Main {
     public static void main(String[] args) {
 
-        FlightService flightService = new FlightServiceImpl();
+        FlightService flightService = new FlightServiceImpl(new FlightBuilder());
 
-        List<FlightFiltrationCriterion> departureBeforeNow = List.of(new DepartureBeforeNowCriterion());
+        List<FlightFiltrationCriterion> departureBeforeNow = List.of(new DepartureNotBeforeNowCriterion());
         List<FlightFiltrationCriterion> arrivalBeforeDeparture = List.of(new ArrivalBeforeDepartureCriterion());
-        List<FlightFiltrationCriterion> landTimeMore = List.of(new LandTimeCriterion("more", 120L, 0));
+        List<FlightFiltrationCriterion> landTimeMore = List.of(new LandTimeCriterion(MORE, 120L, 0));
         List<FlightFiltrationCriterion> combinedCriteria = List.of(
-                new DepartureBeforeNowCriterion(),
-                new LandTimeCriterion("more", 120L, 0),
+                new DepartureNotBeforeNowCriterion(),
+                new LandTimeCriterion(MORE, 120L, 0),
                 new ArrivalBeforeDepartureCriterion());
 
         System.out.println("Flights are" + flightService.getAllFlights());
@@ -33,7 +34,7 @@ public class Main {
         System.out.println("\nFlights without arrival before departure "
                 + flightService.getFlightsByCriteria(arrivalBeforeDeparture));
 
-        System.out.println("\nFlights without more than 2 hours on land "
+        System.out.println("\nFlights without segments including more than 2 hours on land "
                 + flightService.getFlightsByCriteria(landTimeMore));
 
         System.out.println("\nFlights after combined criteria "
