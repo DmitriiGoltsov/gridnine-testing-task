cc2c663eafa5572768ac62a355911679c1689242
dmitrii goltsov <dmitriy.goltsov@mail.ru>
2024-01-12 11:47:47 +0100

    fix: optimise code by creating a united util method (instead of two) for checking the sufficiency of segments to the given parameters

--- src/main/java/com/gridnine/testing/util/FlightUtil.java
+++ src/main/java/com/gridnine/testing/util/FlightUtil.java
@@ -8,43 +8,20 @@ import java.util.List;
 
 public class FlightUtil {
 
-    public static boolean isLandTimeExceeded(List<Segment> segments, long max) {
+    public static boolean doesLandTimeSuffice(List<Segment> segments, long min, long max) {
 
         if (segments.size() <= 1) {
             return true;
         }
 
         long counter = 0L;
-
         for (int i = 0; i < segments.size() - 1; i++) {
             LocalDateTime arrivalTime = segments.get(i).getArrivalDate();
             LocalDateTime departureTime = segments.get(i + 1).getDepartureDate();
 
             counter += Duration.between(arrivalTime, departureTime).toMinutes();
 
-            if (counter > max) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    public static boolean isLandTimeLessThanNeeded(List<Segment> segments, long min) {
-
-        if (segments.size() <= 1) {
-            return false;
-        }
-
-        long counter = 0L;
-
-        for (int i = 0; i < segments.size() - 1; i++) {
-            var arrivalTime = segments.get(i).getArrivalDate();
-            var departureTime = segments.get(i + 1).getDepartureDate();
-
-            counter += Duration.between(arrivalTime, departureTime).toMinutes();
-
-            if (counter < min) {
+            if (counter > max || counter < min) {
                 return false;
             }
         }
